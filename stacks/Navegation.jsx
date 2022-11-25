0ded3d5d06d138f1544df20a358d3c0f695d256d
Carlos Alberto Santos <carlosalbertosantos@uts.edu.co>
2022-11-24 20:45:26 -0500

    Implementacion de los arrays y contenido del producto renderizado.

--- stacks/Navegation.jsx
+++ stacks/Navegation.jsx
@@ -1,37 +1,24 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
-import Login from '../components/Login';
-import Registro from '../components/Registro';
-import PantallaPrincipal from '../components/PantallaPrincipal';
-import Inicio from '../components/Inicio';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import Login from "../components/Login";
+import Registro from "../components/Registro";
+import PantallaPrincipal from "../components/PantallaPrincipal";
+import Inicio from "../components/Inicio";
 
 const Stack = createNativeStackNavigator();
 
-const Navegation = () =>
-{
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-            <Stack.Screen
-                    name = 'Inicio'
-                    component={Inicio}
-                />
-                <Stack.Screen
-                    name = 'Registro'
-                    component={Registro}
-                />
-                    <Stack.Screen
-                        name = 'Pantalla Principal'
-                        component={PantallaPrincipal}
-                    />
-                <Stack.Screen
-                    name = 'Login'
-                    component={Login}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
-}
+const Navegation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Inicio" component={Inicio} />
+        <Stack.Screen name="Registro" component={Registro} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Pantalla Principal" component={PantallaPrincipal} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
-export default Navegation
+export default Navegation;
